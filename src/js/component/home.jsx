ff4b6e58df377e814ad34d7491c5c2f662f65ae7
juanmism <juanmism@gmail.com>
2022-01-26 18:15:12 +0000

    endExercice

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todoList, setTodolist] = useState([]);
+	const [todo, setTodo] = useState("");
+
+	useEffect(() => {
+		console.log("EN EL USE EFFECT");
+	}, []);
+
+	const handelChange = (event) => {
+		console.log(event.target.name);
+		setTodo(event.target.value);
+	};
+	console.log(todo);
+
+	const addNewTodo = (e) => {
+		e.preventDefault();
+		console.log("submit");
+		setTodolist([...todoList, todo]);
+	};
+
+	const DeleteItems = (indexItem) => {
+		setTodolist((prevState) =>
+			prevState.filter((todoList, index) => index !== indexItem)
+		);
+	};
+
+	console.log(todoList);
+
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<form onChange={handelChange} onSubmit={addNewTodo}>
+				<input name="new-todo" type="text"></input>
+				<button type="submit"> save todo </button>
+			</form>
+			<ul>
+				{todoList.map((todoPrint, index) => (
+					<li key={index}>
+						{todoPrint}
+						<span onClick={() => DeleteItems(index)}>X</span>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
